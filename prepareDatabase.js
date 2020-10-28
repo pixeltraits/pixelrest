fc72f61de2505b18ea2c5ef2767c9b1d05f05e2e
Erwan Moreau <erwan.moreau.ouest@gmail.com>
2020-10-28 14:09:27 +0100

    correctif bddParser

--- prepareDatabase.js
+++ prepareDatabase.js
@@ -3,7 +3,5 @@ import usersRepository from './app/repositories/users.repository.js';
 import MysqlParser from 'node-rest/mysqlParser';
 
 const userRepository = new usersRepository(mysql, MysqlParser);
-console.log('lul')
 userRepository.createDataBase().then(() => {
-  console.log('lol')
 });
